d7a0b993f84533fed3b011f84871100b0299ee0d
staceywai <staceywai@Staceys-MacBook-Pro.local>
2021-01-19 20:02:24 -0800

    update HW

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # plotly-challenge
+## Belly Butoon Biodiversity
+
+Built an interactive dashboard to explore the [Belly Button Biodiversity dataset](http://robdunnlab.com/projects/belly-button-biodiversity/), which catalogs the microbes that colonize human navels.
+
+The dataset reveals that a small handful of microbial species (also called operational taxonomic units, or OTUs, in the study) were present in more than 70% of people, while the rest were relatively rare.
+
+## Step 1: Plotly
+Build Dashboard anlayzing "samples" dataset
+
+1. Horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+
+    * values: `sample_values`
+
+    * labels: `otu_ids`
+
+    * hovertext: `otu_labels`
+
+
+2. Bubble chart that displays each sample.
+
+    * x values: `otu_ids`
+
+    * y values: `sample_values`
+
+    * marker size: `sample_values`
+
+    * marker colors: `otu_ids`
+
+    * text values: `otu_labels`
+
+3. Display the sample metadata, i.e., an individual's demographic information.
+
+4. Display each key-value pair from the metadata JSON object somewhere on the page.
+
+5. Update all of the plots any time that a new sample is selected.
+
+## Advanced Challenge Assignment (Optional)
+
+The following task is advanced and therefore optional.
+
+* Adapt the Gauge Chart from <https://plot.ly/javascript/gauge-charts/> to plot the weekly washing frequency of the individual.
+
+* You will need to modify the example gauge code to account for values ranging from 0 through 9.
+
+* Update the chart whenever a new sample is selected.
+
+
+### About the Data
+
+Hulcr, J. et al.(2012) _A Jungle in There: Bacteria in Belly Buttons are Highly Diverse, but Predictable_. Retrieved from: [http://robdunnlab.com/projects/belly-button-biodiversity/results-and-data/](http://robdunnlab.com/projects/belly-button-biodiversity/results-and-data/)
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,126 @@
-// // Use d3.json() to fetch data from JSON file
-// d3.json("samples.json").then((data)=> {}
-// console.log(data);
+function getPlots(id) {
+    //Read JSON data
+    d3.json('samples.json').then(data =>{
 
-// Incoming data is internally referred to as incomingData
-d3.json("data/samples.json").then((importedData) => {
-    console.log(importedData),
-    var data = importedData;
-  };
+        var samples = data.samples.filter(s => s.id.toString() === id)[0];
+        
+        console.log(samples);
+  
+        // Getting the top 10 
+        var samplevalues = samples.sample_values.slice(0, 10).reverse();
+  
+        // get only top 10 otu ids for the plot OTU and reversing it. 
+        var OTU_top = (samples.otu_ids.slice(0, 10)).reverse();
+        
+        // get the otu id's to the desired form for the plot
+        var OTU_id = OTU_top.map(d => "OTU " + d)
+  
+      //   console.log(`OTU IDS: ${OTU_id}`)
+  
+  
+        // get the top 10 labels for the plot
+        var labels = samples.otu_labels.slice(0, 10);
+  
+      //   console.log(`Sample Values: ${samplevalues}`)
+      //   console.log(`Id Values: ${OTU_top}`)
+
+        var trace = {
+            x: samplevalues,
+            y: OTU_id,
+            text: labels,
+            marker: {
+                color: 'blue'},
+            type: "bar",
+            orientation: "h"
+        };
+    //create data variable
+        var data =[trace];
+        
+    //Create layout for plot
+        var layout ={
+            title: "Top 10 OTU",
+            yaxis:{
+                tickmode:"linear"
+            },
+            margin: {
+                l:100,
+                r:100,
+                t:100,
+                b:30
+            }
+        };
+    
+    //create bar plot
+    Plotly.newPlot("bar",data,layout);
+
+    //Trace bubble chart
+        var trace1 = {
+            x:samples.otu_ids,
+            y:samples.sample_values,
+            mode: 'markers',
+            marker: {
+                size: samples.sample_values,
+                color: samples.otu_ids
+            },
+            text: samples.otu_labels
+        };
+    //Create layout for bubble plot
+        var layout1 = {
+            xaxis: {title: "OTU ID"},
+            height: 600,
+            width: 1000
+        };
+    //create bubble plot data variable
+        var data1=[trace1];
+    Plotly.newPlot('bubble',data1,layout1);    
+
+    });
+}
+
+//Create data gather function
+function getDemoInfo(id) {
+    //Read the json files
+    d3.json('data/samples.json').then ((data)=>{
+        var metadata=data.metadata;
+        console.log(metadata)
+
+        //filter metadata by id
+        var result = metadata.filter(meta=>meta.id.toString()===id)[0];
+
+        var demo = d3.select('#sample-metadata');
+
+        demo.html("");
+
+        Object.entries(result).forEach((key)=> {
+            demo.append("h5").text(key[0].toUpperCase()+": " + key[1] + "\n");
+
+        });
+
+    });
+}
+
+//create function for the change event
+function optionChanged(id) {
+    getPlots(id);
+    getDemoInfo(id);
+    buildGauge(id);
+
+}
+
+function init() {
+    var dropdown = d3.select("#selDataset");
+
+    d3.json("data/samples.json").then((data)=> {
+        console.log(data)
+
+        data.names.forEach(function(name){
+            dropdown.append("option").text(name).property("value");
+        });
+
+        getPlots(data.names[0]);
+        getDemoInfo(data.names[0]);
+        buildGauge(data.names[0]);
+    });
+}
+
+init();
